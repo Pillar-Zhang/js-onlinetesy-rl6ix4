7175096d180b6d1434e236f623d36667aa968222
Pillar-Zhang <hnhbxiaodong@163.com>
2021-03-26 09:36:52 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-onlinetesy-rl6ix4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-onlinetesy-rl6ix4)
---
+++ index.js
@@ -0,0 +1,9 @@
+// Import stylesheets
+import './style.css';
+import "./timeFormat.js"
+// import "./mapData.js"
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `hello`;
+
